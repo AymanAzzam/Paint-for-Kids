6fffb41061dc74789ebf0b958bb2654940db93dd
Ayman Azzam <aymanazzam63@gmail.com>
2019-10-10 19:45:46 +0200

    my project

---
+++ Actions/ExitAction.cpp
@@ -0,0 +1,13 @@
+#include "ExitAction.h"
+
+ExitAction::ExitAction(ApplicationManager * pApp): Action(pApp)
+{}
+
+void ExitAction::ReadActionParameters()
+{
+}
+
+//Execute the action
+void ExitAction::Execute()
+{
+}
